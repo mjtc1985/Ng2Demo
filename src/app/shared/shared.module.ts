a83530b6aae60ab245015da623d4da216a315319
Manuel Talavera <manueljesus.talavera@innocv.com>
2018-01-13 12:39:18 +0100

    add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+
+    ],
+    exports: [
+
+    ],
+    providers: [
+
+    ],
+  })
+  export class SharedModule { }

4b9012bfa571f8cf1100c69f92bf55ae342eea4d
Manuel Talavera <manueljesus.talavera@innocv.com>
2018-01-13 13:04:56 +0100

    share shared module to app module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
     declarations: [
-        HeaderComponent
+        HeaderComponent,
+        ListComponent
     ],
     imports: [
 
     ],
     exports: [
-
+        HeaderComponent,
+        ListComponent
     ],
     providers: [
 
